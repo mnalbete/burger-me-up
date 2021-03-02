6d344eadea71cd9fb33a48ea261200e9d9396ea7
mathew <mnalbete@uw.edu>
2021-03-01 17:00:12 -0800

    moved files

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require("./connection.js.js");
+var connection = require("./connection.js.js.js");
 // Helper function for SQL syntax.
 // Let's say we want to pass 3 values into the mySQL query.
 // In order to write the query, we need 3 question marks.
